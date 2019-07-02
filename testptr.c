6325cf908a52978599c22fd8c2256c733187d742
Joakker <joaquinandresleon108@gmail.com>
2019-07-02 17:10:22 -0400

    :D

--- testptr.c
+++ testptr.c
@@ -4,17 +4,26 @@
 
 int main(){
   int numEntr =  count_entries();
+  int guards[26];
+  char dummie;
   Entrada*    database = (Entrada*) malloc(numEntr * sizeof(Entrada));
   Apuntador*  directorio[26];
 
   FILE* diccionario = fopen("txt/diccionario.txt", "r");
   for (int i = 0 ; i < numEntr; i++)
-    fscanf(diccionario, "%[^:] %[^\n]", database[i].palabra, database[i].definicion);
+    fscanf(diccionario, "%[^:] %c %[^\n] %c", database[i].palabra, &dummie, database[i].definicion, &dummie);
 
   for (int i = 0; i < 26 ; ++i)
     directorio[i] = (Apuntador*) malloc(numEntr * sizeof(Apuntador));
 
-  printf("%s\n", database[0].definicion);
+  for (int i = 0; i < 26 ; ++i)
+    guards[i] = 0;
+
+
+  for (int i = 0; i < numEntr; ++i) {
+    char lin = database[i].palabra[0] - 'A';
+    printf("%c %d\n", database[i].palabra[0], lin);
+  }
 
   for (int i = 0 ; i < 26 ; ++i)  free(directorio[i]);
 
